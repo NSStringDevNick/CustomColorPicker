0815a81c8234c74697264d5f3e21a6bf5f720085
browserAmigo <81185167+browseramigo@users.noreply.github.com>
2022-03-28 12:29:38 +0300

    Initial Commit

---
+++ CustomColorPicker/CustomColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomColorPickerApp.swift
+//  CustomColorPicker
+//
+//  Created by Mikita  on 28/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
